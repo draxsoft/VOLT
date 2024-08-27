f55ce81169ed4474ad2b4709c64624a5643a7615
draxsoft <frankkostine@gmail.com>
2024-08-27 18:36:30 -0500

    Delete main.lua

--- main.lua
+++
@@ -1,7 +0,0 @@
--- Test Script for Saving Files to a Non-Allowed Folder
-
--- Attempt to save a file to a non-allowed folder
-local nonAllowedFolderPath = "non-allowed-folder/testfile.txt"
-local fileContent = "This is a test file content."
-
-writefile(nonAllowedFolderPath, fileContent)
